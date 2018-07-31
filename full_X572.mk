2f407605dc660ed91d53f8bba1b035b166f3a049
dumbDevpr <dumbdevpr@yahoo.com>
2018-07-31 00:52:07 +0000

    Open TWRP for X572 @dumbDevpr|@iykequame

--- full_X572.mk
+++ full_X572.mk
@@ -5,11 +5,6 @@ LOCAL_PATH := device/Infinix/X572
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core.mk)
 
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/init.recovery.mt6735.rc:root/init.recovery.mt6735.rc \
-    $(LOCAL_PATH)/ueventd.mt6735.rc:root/ueventd.mt6735.rc
-
 # Ramdisk
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery/root/init.recovery.mt6735.rc:root/init.recovery.mt6735.rc \
